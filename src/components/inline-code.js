4df536ff103ab92250dacbb2654d1f1ac8c12a0c
Haukur Páll Hallvarðsson <hph@hph.is>
2017-12-29 01:43:07 +0000

    Prevent inline code snippets from breaking into multiple lines

--- src/components/inline-code.js
+++ src/components/inline-code.js
@@ -10,6 +10,7 @@ const InlineCode = glamorous('code', { propsAreCssOverrides: true })({
   fontFamily: monoFonts.join(', '),
   fontSize: `${14 / 16}rem`,
   fontStyle: 'normal',
+  whiteSpace: 'nowrap',
 });
 
 export default InlineCode;
