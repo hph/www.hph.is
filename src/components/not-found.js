7773d9f8bdb07c5b8249c8013f4a24f537274edb
Haukur Páll Hallvarðsson <hph@hph.is>
2017-12-29 01:27:20 +0000

    Fix 404 page

--- src/components/not-found.js
+++ src/components/not-found.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
-import { Block, Link, Status, Title } from './shared';
+import { Block, Link, Title } from './shared';
+import Status from './status';
 
 const NotFound = () => (
   <Status code={404}>
     <Title>Page not found</Title>
     <Block marginTop="1rem">
-      <Link href="/">Go home</Link>
+      <Link href="/coding">Go home</Link>
     </Block>
   </Status>
 );
