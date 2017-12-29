46a4d1273c8fdb3b6e5023fd153e49dd7eae8d9e
Haukur Páll Hallvarðsson <hph@hph.is>
2017-12-29 19:47:28 +0000

    Improve anchor tag styling

--- src/components/link.js
+++ src/components/link.js
@@ -1,19 +1,37 @@
 import React from 'react';
 import { Link as ReactRouterLink } from 'react-router-dom';
+import glamorous, { A } from 'glamorous';
+
+const anchorStyles = {
+  textDecoration: 'none',
+  color: '#0085ff',
+  ':hover': {
+    textDecoration: 'underline',
+  },
+  ':visited': {
+    color: '#9b04e2',
+  },
+};
+
+const StyledRouterLink = glamorous(ReactRouterLink)(anchorStyles);
 
 const Link = props => {
   const { children, href, ...restProps } = props;
   if (href.match(/^http/)) {
     return (
-      <a href={href} target="_blank" rel="noopener noreferrer">
+      <A
+        href={href}
+        target="_blank"
+        rel="noopener noreferrer"
+        css={anchorStyles}>
         {children}
-      </a>
+      </A>
     );
   }
   return (
-    <ReactRouterLink to={href} {...restProps}>
+    <StyledRouterLink to={href} {...restProps}>
       {children}
-    </ReactRouterLink>
+    </StyledRouterLink>
   );
 };
 
