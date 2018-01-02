c7f62fb85f9d9697664c64527e7254ed5ccb1f67
Haukur Páll Hallvarðsson <hph@hph.is>
2018-01-02 01:40:37 +0000

    Prevent inline codeblocks from being justified by parent

--- src/components/inline-code.js
+++ src/components/inline-code.js
@@ -3,8 +3,12 @@ import glamorous from 'glamorous';
 import { monoFonts } from './code';
 
 const InlineCode = glamorous('code', { propsAreCssOverrides: true })({
-  padding: `${2 / 16}rem`,
+  display: 'inline-flex',
+  alignItems: 'center',
+  padding: `0 ${2 / 16}rem`,
   borderRadius: `${2 / 16}rem`,
+  lineHeight: 1.5,
+  hyphens: 'none',
   color: '#141414',
   backgroundColor: '#f6f8fa',
   fontFamily: monoFonts.join(', '),
